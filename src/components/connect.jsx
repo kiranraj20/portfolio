1f167da7cf246abd3155d0363dc8985e537cb51f
kiranraj20 <kiranraj80555@gmail.com>
2024-09-23 22:26:44 +0530

    footer

--- src/components/connect.jsx
+++ src/components/connect.jsx
@@ -90,7 +90,7 @@ const Connect = () => {
             ></textarea>
           </div>
           <button
-            type="button"
+            type="submit"
             className={`w-full ${textClass} bg-blue-500 hover:bg-blue-600 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-2 ${focusClass}`}
           >
             Send Message
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,90 @@
 import React, { useContext } from "react";
 import ThemeContext from "./ThemeContext";
+import Logo from "../assets/icons/logo";
+import Linkdin from "../assets/icons/linkdin";
+import Twitter from "../assets/icons/twitter";
+import Instagram from "../assets/icons/instagram";
+import Mail from "../assets/icons/mail";
+import GitHub from "../assets/icons/ginHub";
 
 const Footer = () => {
-
-  const theme = useContext(ThemeContext)
+  const theme = useContext(ThemeContext);
 
   return (
-    <div className={`flex justify-center items-center ${theme==='dark'?'bg-[#111]':'bg-[#eee]'} `}>
-      Footer
-    </div>
+    <footer
+      className={`flex flex-col justify-center items-center ${
+        theme === "dark" ? "bg-gray-800" : "bg-gray-100"
+      } `}
+    >
+      <p className="pt-3">Kiran Raj © 2024</p>
+      <div className="p-4">
+        <a href="/">
+          <Logo />
+          <span>To Top</span>
+        </a>
+      </div>
+      <ul
+        className={`${
+          theme === "dark" ? "text-[#b0b2c3]" : "text-[#1e1e1e]"
+        } flex gap-6 justify-center items-center pb-5 `}
+      >
+        <li>
+          <a
+            href="https://www.linkedin.com/in/kiran-raj-18708720a"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Linkdin
+              value={theme === "dark" ? "hover:text-white" : "hover:text-black"}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://x.com/Kiranra89877641"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Twitter
+              value={theme === "dark" ? "hover:text-white" : "hover:text-black"}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/_kiran_raj_4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Instagram
+              value={theme === "dark" ? "hover:text-white" : "hover:text-black"}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="mailto:kiranraj80555@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Mail
+              value={theme === "dark" ? "hover:text-white" : "hover:text-black"}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/kiranraj20"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHub
+              value={theme === "dark" ? "hover:text-white" : "hover:text-black"}
+            />
+          </a>
+        </li>
+      </ul>
+    </footer>
   );
 };
 
