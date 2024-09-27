80e20349790a002ed6c3ed1ae3c2da4d294490b8
kiranraj20 <kiranraj80555@gmail.com>
2024-09-27 12:21:16 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import "./App.css";
-import LandingPage from "./components/landingPage.jsx";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import './App.css';
+import LandingPage from './components/landingPage.jsx';
 
 function App() {
   return (
-    <SpeedInsights>
-      <div className="App m-0 p-0 w-screen h-screen overflow-scroll no-scrollbar">
-        <LandingPage />
-      </div>
-    </SpeedInsights>
+    <div className="App m-0 p-0 w-screen h-screen overflow-scroll no-scrollbar">
+      <LandingPage />
+    </div>
   );
 }
 
